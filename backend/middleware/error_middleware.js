b3bf66147900fa874cf9ef8be118095212e70c45
vivek2007 <vivekgadde2007@gmail.com>
2025-12-07 23:29:26 +0530

    Delete backend directory

--- backend/middleware/error_middleware.js
+++
@@ -1,4 +0,0 @@
-export const errorHandler = (err, req, res, next) => {
-  console.error("🔥 ERROR:", err);
-  res.status(500).json({ message: err.message || "Server error" });
-};
--- backend/routes/guest_routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { sendGuestMessage } from "../controllers/guest_controller.js";
-
-const router = express.Router();
-
-router.post("/send", sendGuestMessage);
-
-export default router;
